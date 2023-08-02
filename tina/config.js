1e37b7b267c750a0c4e8b4a68980b4eaedf6bbf2
victoriaprisco <p.victoria@wustl.edu>
2023-08-02 00:33:49 -0400

    pushing graphql changes

--- tina/config.js
+++ tina/config.js
@@ -36,6 +36,7 @@ export default defineConfig({
           {
             type: "string",
             name: "layout",
+            label: "Layout",
             default: "post"
           },
           {
