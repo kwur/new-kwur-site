0a723546c1f0299da3edf1483796e37da4267f09
victoriaprisco <p.victoria@wustl.edu>
2023-08-02 13:47:38 -0400

    update schema

--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '6fff45af5617c185074ef187d67b8a1cc4aca7e4', queries });
+export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/9b365103-638a-43d2-aff5-a20f64830810/github/main', token: '6fff45af5617c185074ef187d67b8a1cc4aca7e4', queries });
 export default client;
   
